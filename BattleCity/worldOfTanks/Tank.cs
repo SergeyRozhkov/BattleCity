4922efd339110a1ca0fc160fffc58574068a96ab
SergeyRozhkov <280899cereza@gmail.com>
2018-10-11 19:11:08 +0700

    dsf

--- BattleCity/worldOfTanks/Tank.cs
+++ BattleCity/worldOfTanks/Tank.cs
@@ -25,7 +25,6 @@ namespace BattleCity.worldOfTanks
             get { return dictDirection[Direction]; }
         }
 
-
         protected bool CheckWay(TankDirection direction, List<IGameObject> gameObjects)
         {
             switch (direction)
@@ -36,20 +35,12 @@ namespace BattleCity.worldOfTanks
                         var bottomLeftPoint = new Point(Location.X - Step, Location.Y + Size.Height); // танк левый нижний угол 
                         foreach (var obj in gameObjects)
                         {
-                            if (this.Equals(obj)) continue;
-                            // проверяем, если припястивие выше - true, иначе false 
-                            var top = topLeftPoint.Y >= obj.Location.Y + obj.Size.Height ? true : false;
-                            // проверяем, если припяствие ниже - true, иначе false 
-                            var bottom = bottomLeftPoint.Y <= obj.Location.Y ? true : false;
-                            if (top || bottom) continue; // если припяствие находится вверху или внизу - начинаем заново итерацию 
-                            else
-                            {
-                                var objRightPointX = obj.Location.X + obj.Size.Width;
-                                if (topLeftPoint.X < objRightPointX) // проверяем наличие объекта в переди 
-                                    continue;
-                                if (topLeftPoint.X - objRightPointX >= 0) // проверяем наличие расстояние до объекта 
-                                    continue;
-                            }
+                            if (this.Equals(obj)) continue
+                            var top = topLeftPoint.Y >= obj.Location.Y + obj.Size.Height;
+                            var bottom = bottomLeftPoint.Y <= obj.Location.Y;
+                            var left = topLeftPoint.X >= obj.Location.X + obj.Size.Width;
+                            var right = topLeftPoint.X + Size.Width <= obj.Location.X;
+                            if (top || bottom || left || right) continue; // если припяствие находится вверху или внизу - начинаем заново итерацию 
                             return false; // если никакое условие не совпадает - false 
                         }
                         return true;
@@ -61,19 +52,11 @@ namespace BattleCity.worldOfTanks
                         foreach (var obj in gameObjects)
                         {
                             if (this.Equals(obj)) continue;
-
-                            var left = topLeftPoint.X >= obj.Location.X + obj.Size.Width ? true : false;
-                            var right = topRightPoint.X <= obj.Location.X ? true : false;
-
-                            if (left || right) continue;
-                            else
-                            {
-                                var objBottomPointY = obj.Location.Y + obj.Size.Height;
-                                if (topLeftPoint.Y < objBottomPointY)
-                                    continue;
-                                if (topLeftPoint.Y - objBottomPointY >= 0)
-                                    continue;
-                            }
+                            var left = topLeftPoint.X >= obj.Location.X + obj.Size.Width;
+                            var right = topRightPoint.X <= obj.Location.X;
+                            var top = topLeftPoint.Y >= obj.Location.Y + obj.Size.Height;
+                            var bottom = topLeftPoint.Y + Size.Height <= obj.Location.Y;
+                            if (left || right || top || bottom) continue;
                             return false;
                         }
                         return true;
@@ -85,19 +68,11 @@ namespace BattleCity.worldOfTanks
                         foreach (var obj in gameObjects)
                         {
                             if (this.Equals(obj)) continue;
-
-                            var top = topRightPoint.Y >= obj.Location.Y + obj.Size.Height ? true : false;
-                            var bottom = bottomRightPoint.Y <= obj.Location.Y ? true : false;
-
-                            if (top || bottom) continue;
-                            else
-                            {
-                                var objLeftPointX = obj.Location.X;
-                                if (topRightPoint.X > objLeftPointX)
-                                    continue;
-                                if (objLeftPointX - topRightPoint.X >= 0)
-                                    continue;
-                            }
+                            var top = topRightPoint.Y >= obj.Location.Y + obj.Size.Height;
+                            var bottom = bottomRightPoint.Y <= obj.Location.Y;
+                            var right = topRightPoint.X <= obj.Location.X;
+                            var left = topRightPoint.X >= obj.Location.X + obj.Location.X;
+                            if (top || bottom || left || right) continue;
                             return false;
                         }
                         return true;
@@ -109,19 +84,11 @@ namespace BattleCity.worldOfTanks
                         foreach (var obj in gameObjects)
                         {
                             if (this.Equals(obj)) continue;
-
-                            var left = bottomLeftPoint.X >= obj.Location.X + obj.Size.Width ? true : false;
-                            var right = bottomRightPoint.X <= obj.Location.X ? true : false;
-
-                            if (left || right) continue;
-                            else
-                            {
-                                var objTopPointY = obj.Location.Y;
-                                if (bottomLeftPoint.Y > objTopPointY)
-                                    continue;
-                                if (objTopPointY - bottomLeftPoint.Y >= 0)
-                                    continue;
-                            }
+                            var left = bottomLeftPoint.X >= obj.Location.X + obj.Size.Width;
+                            var right = bottomRightPoint.X <= obj.Location.X;
+                            var top = bottomLeftPoint.Y - Size.Height >= obj.Location.Y + obj.Size.Height;
+                            var bottom = bottomLeftPoint.Y <= obj.Location.Y;
+                            if (left || right || top || bottom) continue;
                             return false;
                         }
                         return true;
